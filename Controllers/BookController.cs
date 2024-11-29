5c2e19c2d34cd211ef9066884387ade5efc9568e
Daniel220903 <dvillalobos878@gmail.com>
2024-11-28 21:25:50 -0600

    uploaded?

---
+++ Controllers/BookController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bogus;
+using System.Collections.Generic;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Bogus.DataSets;
+
+namespace BookApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private static string CurrentLanguage = "en";
+        private static int CurrentSeed = 42;
+        private static decimal AverageLikes = 3.7m;
+        private static decimal AverageReviews = 2.5m;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateBooks(int page = 1) {
+            var books = new List<dynamic>();
+
+            int roundedLikesFloor = (int)Math.Floor(AverageLikes);
+            int roundedLikesCeiling = (int)Math.Ceiling(AverageLikes);
+
+            int roundedReviewsFloor = (int)Math.Floor(AverageReviews);
+            int roundedReviewsCeiling = (int)Math.Ceiling(AverageReviews);
+
+            for (int i = 1; i <= 10; i++){
+                var reviewsList = new List<string>();
+
+                int likesIterations = roundedLikesFloor == roundedLikesCeiling
+                    ? roundedLikesFloor
+                    : new Random(CurrentSeed + i).Next(0, 2) == 0
+                        ? roundedLikesFloor
+                        : roundedLikesCeiling;
+
+                int reviewsIterations = roundedReviewsFloor == roundedReviewsCeiling
+                    ? roundedReviewsFloor
+                    : new Random(CurrentSeed + i * 100).Next(0, 2) == 0
+                        ? roundedReviewsFloor
+                        : roundedReviewsCeiling;
+
+                for (int j = 0; j < reviewsIterations; j++){
+                    Randomizer.Seed = new Random(CurrentSeed + (i * 1000) + j);
+                    string resultado = GenerateReview();
+                    reviewsList.Add(resultado);
+                }
+
+                Randomizer.Seed = new Random(CurrentSeed + (page * 100) + i);
+                var faker = new Faker(CurrentLanguage);
+
+                int index = (page - 1) * 10 + i;
+                books.Add(new {
+                    Index = index,
+                    ISBN = faker.Random.Replace("###-#-##-######-#"),
+                    Title = faker.Lorem.Sentence(),
+                    Author = faker.Name.FullName(),
+                    Publisher = faker.Company.CompanyName(),
+                    Likes = likesIterations,
+                    Reviews = reviewsIterations,
+                    ReviewList = reviewsList,
+                    CoverImage = faker.Image.LoremFlickrUrl()
+                });
+            }
+
+            return Json(books);
+        }
+
+        private string GenerateReview(){
+            var faker = new Faker(CurrentLanguage);
+            string reviewText = faker.Lorem.Sentence(10);
+            string reviewAuthor = faker.Name.FullName();
+            return $"Review by {reviewAuthor}: <br>{reviewText}";
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSettings(string language, int seed, decimal likes, decimal reviews){
+            CurrentLanguage = language;
+            CurrentSeed = seed;
+            AverageLikes = likes;
+            AverageReviews = reviews;
+
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ Controllers/tryController.cs
@@ -0,0 +1,106 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+//AQUI ESTAS PONIENDO LAS PRUEBAS NOMAS
+namespace BookApp.Controllers
+{
+    public class tryController : Controller
+    {
+        private static string CurrentLanguage = "en";
+        private static int CurrentSeed = 42;
+        private static decimal AverageLikes = 3.7m;
+        private static decimal AverageReviews = 2.5m;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateBooks(int page = 1)
+        {
+            Randomizer.Seed = new Random(CurrentSeed + page);
+            var faker = new Faker(CurrentLanguage);
+
+            var books = new List<dynamic>();
+
+            int roundedLikesFloor = 0;
+            int roundedLikesCeiling = 0;
+
+            if (AverageLikes % 1 == 0)
+            {
+                roundedLikesFloor = roundedLikesCeiling = (int)AverageLikes;
+            }
+            else
+            {
+                roundedLikesFloor = (int)Math.Floor(AverageLikes);
+                roundedLikesCeiling = (int)Math.Ceiling(AverageLikes);
+            }
+
+            int roundedReviewsFloor = 0;
+            int roundedReviewsCeiling = 0;
+            if (AverageReviews % 1 == 0)
+            {
+                roundedReviewsFloor = roundedReviewsCeiling = (int)AverageReviews;
+            }
+            else
+            {
+                roundedReviewsFloor = (int)Math.Floor(AverageReviews);
+                roundedReviewsCeiling = (int)Math.Ceiling(AverageReviews);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var reviewsList = new List<string>();
+
+                int reviewsIterations = roundedReviewsFloor == roundedReviewsCeiling
+                    ? roundedReviewsFloor
+                    : new Random().Next(0, 2) == 0
+                        ? roundedReviewsFloor
+                        : roundedReviewsCeiling;
+
+                for (int j = 0; j < reviewsIterations; j++){
+                    string reviewText = faker.Lorem.Sentence(10);
+                    string reviewAuthor = faker.Name.FullName();
+
+                    reviewsList.Add($"Review by {reviewAuthor}: <br>{reviewText}");
+                }
+
+                int likesIterations = roundedLikesFloor == roundedLikesCeiling
+                    ? roundedLikesFloor
+                    : new Random().Next(0, 2) == 0
+                        ? roundedLikesFloor
+                        : roundedLikesCeiling;
+
+                int index = (page - 1) * 10 + i;
+                books.Add(new
+                {
+                    Index = index,
+                    ISBN = faker.Random.Replace("###-#-##-######-#"),
+                    Title = faker.Hacker.Adjective() + " " + faker.Vehicle.Manufacturer(),
+                    Author = faker.Name.FullName(),
+                    Publisher = faker.Company.CompanyName(),
+                    Likes = likesIterations,
+                    Reviews = reviewsIterations,
+                    ReviewList = reviewsList,
+                    CoverImage = faker.Image.LoremFlickrUrl()
+                });
+            }
+
+            return Json(books);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateSettings(string language, int seed, decimal likes, decimal reviews)
+        {
+            CurrentLanguage = language;
+            CurrentSeed = seed;
+            AverageLikes = likes;
+            AverageReviews = reviews;
+
+            return Json(new { success = true });
+        }
+    }
+}
